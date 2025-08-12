c7ed792a9b1af529143de9f2cf3f6126fb58da5d
Bryan <bryanhh.lb@gmail.com>
2025-08-11 22:16:17 -0700

    Constants for Motor Ports

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+public class Constants {
+    public static int LeftDrivePort = 1;
+    public static int rightDrivePort = 0;
+
+    public static int shooterIndexerPort = 2;
+    public static int shooterAPort = 1;
+    public static int shooterBPort = 7;
+    public static int shooterHoodPort = 8;
+
+    public static int addressableLEDPort = 9;
+}
