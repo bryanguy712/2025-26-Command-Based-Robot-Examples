1831845b9bcb0bd4251576dd69cd4bbd170a89ba
Bryan <bryanhh.lb@gmail.com>
2025-10-15 22:08:28 -0700

    Reworked Drive Command Logic

--- src/main/java/frc/robot/commands/TeleopDriveCommand.java
+++ src/main/java/frc/robot/commands/TeleopDriveCommand.java
@@ -1,18 +1,26 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class TeleopDriveCommand extends Command{
     private DriveSubsystem driveSubsystem;
 
-    public TeleopDriveCommand() {
+    private XboxController controller;
+
+    public TeleopDriveCommand(DriveSubsystem driveSubsystem, XboxController controller) {
+        this.driveSubsystem = driveSubsystem;
+        this.controller = controller;
+
         addRequirements(driveSubsystem);
     }
 
     @Override
     public void execute() {
-        driveSubsystem.tankDrive();
+        double moveRequest = controller.getLeftY();
+        double turnRequest = controller.getRightX();
+        driveSubsystem.arcadeDrive(moveRequest, turnRequest);
     }
 
     @Override
